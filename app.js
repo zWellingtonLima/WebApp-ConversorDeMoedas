2763dc3f52f9db340ed65a3b1267e542f5b9134b
zWellingtonLima <0wellingtonlima1@gmail.com>
2022-09-30 18:14:49 +0100

    Link da API incluído e função async await com um try catch estabelecida

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const currencyElOne = document.querySelector('[data-js="currency-one"]');
-const currencyElTwo = document.querySelector('[data-js="currency-two"]');
+const currencyOneEl = document.querySelector('[data-js="currency-one"]');
+const currencyTwoEl = document.querySelector('[data-js="currency-two"]');
+
+const url = 'https://v6.exchangerate-api.com/v6/1a12cce673f9a34231d428a3/latest/USD';
+
+const fetchExchangeRate = async () => {
+  try {
+    const response = await fetch(url)
+    console.log(response);
+    
+  } catch (err) {
+
+  }
+}
+fetchExchangeRate()
 
 const option = `<option>oi</option>`;
 
-currencyElOne.innerHTML = option;
-currencyElTwo.innerHTML = option;
+currencyOneEl.innerHTML = option;
+currencyTwoEl.innerHTML = option;
+
+console.log(currencyOneEl, currencyTwoEl);
+
